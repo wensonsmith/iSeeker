024f2c44340ba0f2fbcc20fe049caaa520a6da82
WensonSmith <wensonsmith@gmail.com>
2014-01-09 17:45:51 +0800

    Change Template Engine To doT, Remove Bootstrap

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var Settings = require('./settings');
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'doT');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
